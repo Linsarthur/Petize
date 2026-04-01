f5403ebc277fc548c24838f220290c0ed823da53
Arthur Cavalcante <linsarthur02@gmail.com>
2026-04-01 18:16:42 -0300

    updating files

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -3,17 +3,33 @@ import type { userProps } from "../types/user";
 
 type UserContextType = {
   user: userProps | null;
+  repos: Repo[];
+  setRepos: React.Dispatch<React.SetStateAction<Repo[]>>; 
   loadUser: (userName: string) => Promise<void>;
+};
+
+export type Repo = {
+  id: number;
+  name: string;
+  description: string;
+  stargazers_count: number;
+  updated_at: string;
+  html_url: string;
 };
 
 const UserContext = createContext<UserContextType | null>(null);
 
 export const UserProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<userProps | null>(null);
-
+  const [repos, setRepos] = useState<Repo[]>([]);
   const loadUser = async (userName: string) => {
     const res = await fetch(`https://api.github.com/users/${userName}`);
     const data = await res.json();
+    const reposRes = await fetch(
+      `https://api.github.com/users/${userName}/repos?per_page=10&page=1&sort=updated`,
+    );
+    const reposData = await reposRes.json();
+    setRepos(reposData);
 
     setUser({
       avatar_url: data.avatar_url,
@@ -31,13 +47,14 @@ export const UserProvider = ({ children }: { children: React.ReactNode }) => {
   };
 
   return (
-    <UserContext.Provider value={{ user, loadUser }}>
+    <UserContext.Provider value={{ user, repos, setRepos, loadUser }}>
+      {" "}
+      {}
       {children}
     </UserContext.Provider>
   );
 };
 
-// Hook customizado para facilitar o uso
 export const useUser = () => {
   const context = useContext(UserContext);
   if (!context) throw new Error("useUser must be used within a UserProvider");
