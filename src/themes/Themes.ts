b9d39387821d40976e0c6d0d7307bb0cae88c928
Arthur Cavalcante <linsarthur02@gmail.com>
2026-03-31 23:09:17 -0300

    profile page static done

--- src/themes/Themes.ts
+++ src/themes/Themes.ts
@@ -3,6 +3,9 @@ import { extendTheme } from "@chakra-ui/react";
 interface BrandColors {
   primary: string;
   secondary: string;
+  grey: string;
+  colorInfoUser: string;
+  borderColor: string;
 }
 
 interface ThemeColors {
@@ -24,6 +27,9 @@ const customTheme: CustomThemeConfig = {
     brand: {
       primary: "#0069CA",
       secondary: "#8C19D2",
+      grey: "#A0AEC0",
+      colorInfoUser: "#4A5568",
+      borderColor: "#E2E8F0",
     },
   },
   fonts: {
