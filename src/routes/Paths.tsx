260d0fbde0a8169753c1a2db1552342915b3360a
Arthur Cavalcante <linsarthur02@gmail.com>
2026-04-01 22:36:10 -0300

    fixing for deploy

--- src/routes/Paths.tsx
+++ src/routes/Paths.tsx
@@ -1,18 +1,15 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom"; // ✅ tudo do mesmo pacote
 import Home from "../pages/Home";
 import Profile from "../pages/Profile";
-import { HashRouter } from "react-router";
 
 const Paths = () => {
   return (
-    <>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile/:username" element={<Profile />} />
-        </Routes>
-      </HashRouter>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
+    </HashRouter>
   );
 };
 
